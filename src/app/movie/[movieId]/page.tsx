5db2ec8df3a8ac19219b8bccbf092bbb20d827c7
AB1738 <saintsring2017@yahoo.com>
2025-05-04 17:52:35 -0400

    updated layout for specific movie page

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -32,45 +32,51 @@ const MoviePage = async ({ params }: PropType) => {
   };
 
   return (
-    <div className="flex flex-col gap-2.5 items-center">
+    <div className="flex flex-col gap-3.5 items-center">
       <h1 className="text-3xl font-bold sm:self-start py-2">{movie.title}</h1>
-      <div className="flex flex-col sm:flex-row items-center">
-        <a href={movie.homepage} className="flex-1">
-          <Image
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt={`${movie.title} cover image`}
-            height={1000}
-            width={1000}
-            className="h-[300px] rounded-lg w-full"
-          />
-        </a>
-        <p className="flex-1 font-semibold text-center">{movie.overview}</p>
-      </div>
+      <a href={movie.homepage} className="flex-1">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+          alt={`${movie.title} cover image`}
+          height={1000}
+          width={1000}
+          className="rounded-lg w-full"
+        />
+      </a>
+      <p className="flex-1 font-semibold text-center leading-7">
+        {movie.overview}
+      </p>
+      <div className="flex flex-col sm:flex-row justify-center items-center w-full">
+        <div className="flex flex-col text-center gap-2 flex-1">
+          <h3 className="font-bold text-2xl pb-2">Movie Info</h3>
 
-      <p>
-        Original Language: <span>{movie.original_language.toUpperCase()}</span>
-      </p>
-      <p>
-        Release Date:{" "}
-        <span>{new Date(movie.release_date).toLocaleDateString()}</span>
-      </p>
-      <p>
-        Runtime: <span>{getDuration(movie.runtime)}</span>
-      </p>
-      <div className="flex flex-col items-center">
-        <h3 className="font-bold text-2xl pb-2">Genres</h3>
-        <ul>
-          {movie.genres.map((genre) => (
-            <li key={genre.id} className="py-1">
-              {genre.name}
-            </li>
-          ))}
-        </ul>
+          <p>
+            Original Language:{" "}
+            <span>{movie.original_language.toUpperCase()}</span>
+          </p>
+          <p>
+            Release Date:{" "}
+            <span>{new Date(movie.release_date).toLocaleDateString()}</span>
+          </p>
+          <p>
+            Runtime: <span>{getDuration(movie.runtime)}</span>
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center flex-1">
+          <h3 className="font-bold text-2xl pb-2">Genres</h3>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre.id} className="py-1">
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       {video && video.length > 0 && (
         <iframe
-          className="rounded-4xl w-full md:w-[560px]"
-          height="315"
+          className="rounded-4xl w-full md:w-[560px] aspect-video"
+          //   height="315"
           src={`https://www.youtube.com/embed/${video[0].key}`}
           title={video[0].name}
           frameBorder="0"
