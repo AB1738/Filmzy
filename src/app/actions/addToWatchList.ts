226b2bc1101aa184795139c3e450e9358dad3247
AB1738 <saintsring2017@yahoo.com>
2025-05-05 22:13:29 -0400

    started logic in watchlist server action

--- src/app/actions/addToWatchList.ts
+++ src/app/actions/addToWatchList.ts
@@ -6,6 +6,22 @@ const addToWatchList = async () => {
   try {
     const user = await currentUser();
     if (!user) return; //HANDLE THIS BETTER
+    const userIsInDb = await prisma.user.findUnique({
+      where: {
+        userId: user.id,
+      },
+    });
+    if (!userIsInDb) {
+      const newUser = await prisma.user.create({
+        data: {
+          userId: user.id,
+          firstName: user.firstName || "",
+        },
+      });
+      //watchlist logic for new user then return
+    }
+
+    //watchlist logic for existing user then return
   } catch (e) {
     console.log(e);
   }
